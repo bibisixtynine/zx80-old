fd5b1f3a73e28964d08b9c471d355f0e2db6e0ed
Jérôme Binachon <ilboued@proton.me>
2023-08-30 19:56:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# zx80.app, the online programmable computer
+
+<p align="center">
+  <a href="https://www.zx80.app"><img src="static/homescreen_title.png" height="auto" width="70%"></a>
+</p>
+
+## What's zx80.app ?
+
+WARNING : this app is at its very early stage of development... less than ALPHA !
+
+It looks like a smartphone home-screen, and (will) allow the use, (simplified) development, and (easy) share of webapps.
+
+Although this app works online, it is also designed to be installed locally (it is a PWA). It will be then available offline like a 'classic' app ! (see how at the end of this page)
+
+For the installation process, use :
+- iPod/iPhone/iPad : Apple Safari
+- OSX, Windows, Linux : Google Chrome (or Microsoft Edge)
+
+
+## how to install zx80.app on any platform ?
+
+Here is [a link for some more info on how to install a PWA webapp](https://www.cdc.gov/niosh/mining/content/hearingloss/installPWA.html#:~:text=If%20the%20site%20is%20a,the%20%22three%20dot%22%20menu.)
+
+
+## Tested on...
+
+- iPhone 13 mini (ios 16.2 and Safari)
+- Macbook Air M1 (Chrome 109 and Microsoft Edge 109)
+- 2013 Nexus 7 tablet with android 6.0.1 and Chrome 100
+
+
+## Why this app ?
+
+- Target audience : anyone who would like to learn coding and share apps than can be installed on any platform, even with a 'small' iphone 13 mini (I love to code with my iphone mini 🤓)
+
+- Ease the very begining of the learning curve of coding apps
+
+- No need of developper account, apple store or google store, specific hardware configuration : just a computer, tablet or smartphone with any operating system... able to run Safari (ios) or Google Chrome
+
+- 'modern' javascript is used for coding
+
+
+## Many Thanks to
+
+- Code Mirror 6 and Marijn Haverbeke
+- Svelte and Rich Harris
+- Vite and Evan You
+- all the contributors of these free wonderful open-source tools
+
+and also 
+
+- Stackblitz, Eric Simons and his team for the amazing online Codeflow IDE (in fact... much more in practice)... Waou !
+- Vercel, Guillermo Rauch and his team for it's great web deployement solution
+
+and from my deep heart
+
+- Sir Clive Sinclair and it's zx80,81,spectrum and QL computer family !
+
+
+## Where to go from there ?
+
+press one of these buttons :
+
+[![Launch web app](static/launch-web-app.svg)](https://www.zx80.app)
+
+[![Open in Codeflow](https://developer.stackblitz.com/img/open_in_codeflow.svg)](https:///pr.new/bibisixtynine/zx80)
+ 
+in Codeflow IDE, everything starts in :
+- src/app.html
+- src/routes/+page.svelte
---
+++ src/lib/app-pixel.js
@@ -0,0 +1,213 @@
+import icons from "./database-icons.js";
+
+export default  {
+    name: "Pixel 2",
+    id: 0,
+    svg: icons.boy,
+    backgroundColor: "rgb(0, 0, 255)",
+    script: `
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////
+//
+// 🤖 README FIRST
+//
+//  Use chrome brower debug window for debugging (only on desktop 🥲)
+//    . Press option+command+i on chrome osx,
+//    . Use the 'console' tab to see the errors message while you
+//      are typing code,
+//    . Add 'debugger' instruction (without brackets) to add a breakpoint
+//      anywhere in your code in this page,
+//    . Each time you type a character... the app auto relaunch.
+//
+//  ✅ Done !
+//
+
+//////////////////////////
+//
+// 👾 PIXEL APP 1.0
+//
+
+// some printings
+print('<orange>the <yellow>Pixel App<orange> 1.0')
+
+
+// add a drawing canvas for the pixel app !
+print('<canvas style="border: solid 2px orange; border-radius: 15px; touch-action:none; width:80%" class="rainbow-pixel-canvas"></canvas>')
+
+// some more printings
+print('so happy it works !')
+print('<purple>Yeah Man !</red>')
+print('👀')
+print('my name is John !')
+print('i am your 🤖')
+print('<blue>yes <white>i <red>am')
+print('so nice 🤩')
+print('so happy it works !')
+print('<purple>Yeah Man !</red>')
+
+/////////////////////////////////////////////////////////////
+//
+// the pixel app... could work as it is a static html file...
+//
+// <html>
+//   <head>
+//   </head>
+//   <body>
+//     <script type='module'> ... adding all the content of this file here ! </scr ipt>
+//   </body>
+// </html>
+// 
+// STRANGE BUG : NEVER PUT THE correct</sc ript> in the comment...
+
+
+import interact from 'https://cdn.interactjs.io/v1.10.11/interactjs/index.js';
+
+var pixelSize = 16;
+
+interact('.rainbow-pixel-canvas')
+  .draggable({
+    autoScroll: false,
+    max: Infinity,
+    maxPerElement: Infinity,
+    origin: 'self',
+    modifiers: [
+      interact.modifiers.snap({
+      // snap to the corners of a grid
+        targets: [
+          interact.snappers.grid({ x: pixelSize, y: pixelSize }),
+        ],
+      }),
+    ],
+    listeners: {
+      // draw colored squares on move
+      move: function (event) {
+        var context = event.target.getContext('2d');
+        // calculate the angle of the drag direction
+        var dragAngle =
+          (180 * Math.atan2(event.dx, event.dy)) / Math.PI;
+        // set color based on drag angle and speed
+        context.fillStyle =
+          'hsl(' +
+          dragAngle +
+          ', 86%, ' +
+          (30 + Math.min(event.speed / 1000, 1) * 50) +
+          '%)';
+        // draw squares
+        context.fillRect(
+          event.pageX - pixelSize / 2,
+          event.pageY - pixelSize / 2,
+          pixelSize,
+          pixelSize
+        );
+      },
+    },
+  })
+  // clear the canvas on doubletap
+  .on('doubletap', function (event) {
+    var context = event.target.getContext('2d');
+    context.clearRect(
+      0,
+      0,
+      context.canvas.width,
+      context.canvas.height
+    );
+    resizeCanvases();
+  });
+
+function resizeCanvases() {
+  [].forEach.call(
+    document.querySelectorAll('.rainbow-pixel-canvas'),
+    function (canvas) {
+      delete canvas.width;
+      delete canvas.height;
+      var rect = canvas.getBoundingClientRect();
+      canvas.width = rect.width;
+      canvas.height = rect.height;
+    }
+  );
+}
+
+resizeCanvases();
+ 
+// interact.js can also add DOM event listeners
+interact(window).on('resize', resizeCanvases);
+
+
+/////////////////////////////
+//
+// 📚 libs
+//
+// NB: _ prefixed function are 'private' to 'libs'
+function input(text,fct) {
+  const id=Date.now().toString()
+  printf(text+ '<input id="'+id+'"placeholder="Enter some text" name="name" />')
+  const e = document.getElementById(id)
+  e.addEventListener('change',(event)=>{
+    fct(event.target.value)
+  })
+}
+
+function getUI(uiId) {
+  const ui = document.getElementById(uiId)
+  if (!ui) {
+    const div = document.createElement('div')
+    div.id = uiId
+    document.body.appendChild(div)
+    _initUI(div)
+  }
+  return document.getElementById(uiId)
+} 
+
+function _initUI(ui) {
+  ui.innerHTML = ''
+  ui.innerHTML = '<style>center{position:fixed; left:50%; top:50%; transform:translate(-50%,-50%)} red{color:red} white{color:white} blue{color:blue} green{color:green} yellow{color:yellow} orange{color:orange} purple{color:purple}</style>'
+  ui.style.fontFamily = 'monospace'
+  ui.style.fontSize = '20px'
+  ui.style.position = 'fixed'
+  ui.style.top = '0px'
+  ui.style.left = '0px'
+  ui.style.backgroundColor = 'rgba(0,0,0,0.5)'
+  ui.style.width = '100%'
+  ui.style.height = '100%'
+  ui.style.overflow = 'scroll'
+  ui.style.color = 'green'
+  ui.style.textAlign = 'center'
+  ui.style.borderRadius = '25px'
+  ui.style.margin = '0px'
+  ui.style.padding = '0px'
+  return ui
+}
+
+function clear(uiId = 'ui') {
+  const ui = getUI(uiId)
+  ui.innerHTML = ''
+  ui.innerHTML = '<style>center{position:fixed; left:50%; top:50%; transform:translate(-50%,-50%)} red{color:red} white{color:white} blue{color:blue} green{color:green} yellow{color:yellow} orange{color:orange} purple{color:purple}</style>'
+}
+  
+function printf(html, uiId = 'ui') {
+  const ui = getUI(uiId)
+  ui.insertAdjacentHTML('beforeend', html);
+}
+
+function print(html, uiId = 'ui') {
+  const ui = getUI(uiId)
+  ui.insertAdjacentHTML('beforeend', html+"<br>");
+}
+
+
+`}
